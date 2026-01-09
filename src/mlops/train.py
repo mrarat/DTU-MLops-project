855bea5c75a7ed0ad3324482a13be077aebbeeea
avkollerup <s234136@dtu.dk>
2026-01-09 14:24:26 +0100

    adding loss func

--- src/mlops/train.py
+++ src/mlops/train.py
@@ -4,13 +4,14 @@ import typer
 import torch
 
 def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10):
-    dataset = MyDataset("data/raw")
+    train_set, test_set =  # FIXME:missing how to correctly load data 
     model = Model()
-
     train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size)
     test_dataloader = torch.utils.data.DataLoader(test_set, batch_size=batch_size)
 
-    loss_fn = torch.nn.BCELoss()
+    loss_fn = torch.nn.BCELoss() # Using binary cross entropy 
+    s_weight = 1 
+    r_weight = 1
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
     statistics = {"train_loss": [], "train_accuracy": []}
@@ -19,13 +20,14 @@ def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10):
         for i, (img, target) in enumerate(train_dataloader):
             optimizer.zero_grad()
             y_pred = model(img)
-            loss = loss_fn(y_pred, target)
+            loss = s_weight*loss_fn(y_pred['suit'], target['suit']) + r_weight*loss_fn(y_pred['rank'], target['rank'])  # calculating loss as sum of the seperate losses
             loss.backward()
             optimizer.step()
             statistics["train_loss"].append(loss.item())
 
-            accuracy = (y_pred.argmax(dim=1) == target).float().mean().item()
-            statistics["train_accuracy"].append(accuracy)
+            r_accuracy = (y_pred['rank'].argmax(dim=1) == target['rank']).float().mean().item()
+            s_accuracy = (y_pred['suit'].argmax(dim=1) == target['suit']).float().mean().item()
+            statistics["train_accuracy"].append((s_accuracy,r_accuracy))
 
             if i % 100 == 0:
                 print(f"Epoch {epoch}, iter {i}, loss: {loss.item()}")
