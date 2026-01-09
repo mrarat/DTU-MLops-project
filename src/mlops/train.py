89a02f8ea5331d38f2d46cfe22d40bdc4198ebea
JulieTTNguyen <julienguyen454@gmail.com>
2026-01-09 15:45:16 +0100

    WIP train.py

--- src/mlops/train.py
+++ src/mlops/train.py
@@ -1,5 +1,5 @@
 from mlops.model import Model
-from mlops.data import fetch_cards
+from mlops.data import load_data
 import torch
 import hydra
 import os
@@ -12,12 +12,18 @@ DEVICE = torch.device(
     else "cpu"
 )
 
-@hydra.main(version_base = None, config_path = "configs", config_name = "config.yaml")
-def train(cfg):
-    batch_size = cfg.hyperparameters.batch_size
-    epochs = cfg.hyperparameters.epochs
-    lr = cfg.hyperparameters.lr
-    seed = cfg.hyperparameters.seed
+# @hydra.main(version_base = None, config_path = "configs", config_name = "config.yaml")
+# def train(cfg):
+def train():
+    # batch_size = cfg.hyperparameters.batch_size
+    # epochs = cfg.hyperparameters.epochs
+    # lr = cfg.hyperparameters.lr
+    # seed = cfg.hyperparameters.seed
+
+    batch_size = 32
+    epochs = 10
+    lr = 1e-3
+    seed = 42
 
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
@@ -27,12 +33,13 @@ def train(cfg):
                                                      'learning rate':lr,'seed':seed})
 
     # Loading data
-    train_set, test_set = fetch_cards() # FIXME:missing how to correctly load data 
+    train_set = load_data(split = "train")
+    test_set = load_data(split = "test")
     model = Model().to(DEVICE)
     train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
     test_dataloader = torch.utils.data.DataLoader(test_set, batch_size=batch_size)
 
-    loss_fn = torch.nn.CrossEntropyLoss() # Using Cross entropy 
+    loss_fn =  torch.nn.CrossEntropyLoss(ignore_index=-1) # Using Cross entropy 
     rank_weight = 1
     suit_weight = 1 
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
@@ -45,16 +52,17 @@ def train(cfg):
         model.train()
         for i, (img, target) in enumerate(train_dataloader):
             optimizer.zero_grad()
-
+            img = img.float() / 255.0  # convert to float in [0,1]
             img = img.to(DEVICE)
-            target['rank'] = target['rank'].to(DEVICE)
-            target['suit'] = target['suit'].to(DEVICE)
+            print(target)
+            target[:,0] = target[:,0].to(DEVICE) # 0 is rank
+            target[:,1] = target[:,1].to(DEVICE) # 1 is suit
 
             #predict
             y_pred = model(img)
             
             # compute loss
-            loss = suit_weight*loss_fn(y_pred['suit'], target['suit']) + rank_weight*loss_fn(y_pred['rank'], target['rank'])  # calculating loss as sum of the seperate losses
+            loss = suit_weight*loss_fn(y_pred['suit'], target[:,1]) + rank_weight*loss_fn(y_pred['rank'], target[:,0])  # calculating loss as sum of the seperate losses
             # gradient step
             loss.backward()
             optimizer.step()
@@ -62,8 +70,8 @@ def train(cfg):
             # Statistics
             statistics["train_loss"].append(loss.item())
 
-            r_accuracy = (y_pred['rank'].argmax(dim=1) == target['rank']).float().mean().item()
-            s_accuracy = (y_pred['suit'].argmax(dim=1) == target['suit']).float().mean().item()
+            r_accuracy = (y_pred['rank'].argmax(dim=1) == target[:,0]).float().mean().item()
+            s_accuracy = (y_pred['suit'].argmax(dim=1) == target[:,1]).float().mean().item()
 
             statistics["train_accuracy_rank"].append(r_accuracy)
             statistics["train_accuracy_suit"].append(s_accuracy)
